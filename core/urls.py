486f4ebcb244dae94464d4f681a6bc86d51151d1
tomo0111 <tohito0430@gmail.com>
2018-05-18 19:21:45 +0900

    add hello mapping

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+
+from api import hello_view
 from api import item_view
 
 urlpatterns = [
+    url('hello', hello_view.HelloView.as_view(), name='hello'),
     url('items', item_view.ItemView.as_view(), name='items'),
 ]
